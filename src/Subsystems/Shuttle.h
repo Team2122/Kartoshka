eb2b7d11d097933dfe7d54631cfb32e932cd41e6
Daniele <moro.daniele@gmail.com>
2015-02-16 13:07:03 -0700

    Fixes all the documentaation in shuttle

--- src/Subsystems/Shuttle.h
+++ src/Subsystems/Shuttle.h
@@ -18,20 +18,73 @@ namespace tator {
  */
 class Shuttle: public SubsystemBase {
 private:
+
+	/**
+	 * Sensor that senses if totes are here
+	 */
 	DigitalInput* toteSensor;
+
+	/**
+	 * Sensor that senses if Shuttle has reached it's lower limit
+	 */
 	DigitalInput* lowerLimit;
+
+	/**
+	 * Sensor that senses if Shuttle has reached it's upper limit
+	 */
 	DigitalInput* upperLimit;
+
+	/**
+	 * Piston that grabs the totes
+	 */
 	Solenoid* clampPiston;
+
+	/**
+	 * Motor that lifts the shuttle with totes
+	 */
 	Talon* liftMotor;
+
+	/**
+	 * Encoder to read hoe far shuttle has moved
+	 */
 	Encoder* liftEncoder;
+
+	/**
+	 * Power distribution board for detecting if motor has stalled
+	 */
 	PowerDistributionPanel* pdp;
+
+	/**
+	 * Pistons that hold uppermost totes in place
+	 */
 	Solenoid* fingersPiston;
 
+	/**
+	 * This is the speed of the shuttle when going up
+	 */
 	double upSpeed;
+
+	/**
+	 * The speed of the shuttle when going down
+	 */
 	double downSpeed;
+
+	/**
+	 * The speed of the shuttle when holding the totes in place
+	 */
 	double holdSpeed;
+
+	/**
+	 * The value that can be multiplied by how many totes we are holding
+	 * to get the speed we need to counteract that force
+	 */
 	double speedScale;
 	uint8_t motorPDPChannel;
+
+	/**
+	 * The maximum current that can be sent to the shuttle motors
+	 * Is used to know if the motor has stalled
+	 */
 	double maxMotorCurrent;
 
 	/**
@@ -43,14 +96,27 @@ public:
 	Shuttle(YAML::Node config);
 	virtual ~Shuttle();
 
+	/**
+	 * This is the position of the Shuttle itself
+	 * kUpper means it is in its uppermost location
+	 * kLower means it is in its tote-receiving location
+	 */
 	enum Position {
 		kUpper, kLower, kUnknown
 	};
 
+	/**
+	 * This is the direction the shuttle has to move
+	 */
 	enum Speed {
 		kUp, kDown, kHold, kStop
 	};
 
+	/**
+	 * The states of the fingers that hold the uppermost totes in place
+	 * kHeld means that they are stationary
+	 * kReleased means that they allow the totes to move
+	 */
 	enum FingersState {
 		kHeld = 1, kReleased = 0
 	};
@@ -84,6 +150,10 @@ public:
 	 */
 	void SetShuttleSpeed(Speed speed);
 
+	/**
+	 * Set the speed of the shuttle manually, without the enums
+	 * @param speed The rpm the shuttle moves //TODO what units the speed takes in?
+	 */
 	void SetShuttleSpeed(double speed);
 
 	/**
