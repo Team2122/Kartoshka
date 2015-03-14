6a3cb4941291197c801fd2560edd9508e50790ce
amikhalev <alexmikhalevalex@gmail.com>
2015-03-13 23:22:08 -0600

    Hotfix: Actually added command; I am stupid

---
+++ src/Commands/RollerbedPistonToggle.h
@@ -0,0 +1,37 @@
+#include "CommandBase.h"
+
+#ifndef ROLLERBEDPISTONTOGGLE_H_
+#define ROLLERBEDPISTONTOGGLE_H_
+
+#include "CommandBase.h"
+#include "Subsystems/ToteFeed.h"
+
+namespace tator {
+
+class RollerbedPistonToggle: public CommandBase {
+public:
+	RollerbedPistonToggle(std::string name, YAML::Node config) :
+			CommandBase(name) {
+	}
+
+	static std::string GetBaseName() {
+		return "RollerbedPistonToggle";
+	}
+
+protected:
+	void Execute() override {
+		if (toteFeed->GetRollerPiston()) {
+			toteFeed->SetRollerPiston(ToteFeed::RollerPistonState::down);
+		} else {
+			toteFeed->SetRollerPiston(ToteFeed::RollerPistonState::up);
+		}
+	}
+
+	bool IsFinished() {
+		return true;
+	}
+};
+
+}
+
+#endif /* ROLLERBEDPISTONTOGGLE_H_ */
