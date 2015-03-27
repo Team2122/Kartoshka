78e62cfa645d2e64b2f0e23b8be0c93e395ccd47
Zachary Snow <zachary.j.snow@gmail.com>
2015-03-27 00:29:59 -0600

    Revert "Hotfix: Shuttle speed changes because of motor change"
    This reverts commit 31caa4a195cfb5aa0de03629e51a141a8f78f135.

--- src/Commands/ToteDictator/ToteDictator.h
+++ src/Commands/ToteDictator/ToteDictator.h
@@ -165,11 +165,6 @@ protected:
 					this->Cancel();
 				}
 			}
-			// If we have no totes on the ratchets
-			else {
-				// Stop the intake sequence
-				this->Cancel();
-			}
 		}
 
 		// If we have more than the number of totes we want
