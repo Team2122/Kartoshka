578c0884005c4acaad9fa2cf9f4ff7723e164e1b
amikhalev <alexmikhalevalex@gmail.com>
2015-04-08 18:04:50 -0600

    Rewrote ClawEstablish home
     * It doesn't disable the claw if the command runs in auto
     * Made the command simpler

--- src/Commands/Claw/ClawEstablishHome.h
+++ src/Commands/Claw/ClawEstablishHome.h
@@ -3,6 +3,7 @@
 
 #include "CommandBase.h"
 #include "Subsystems/Claw.h"
+#include <DriverStation.h>
 
 namespace tator {
 
@@ -19,25 +20,39 @@ public:
 
 protected:
 	void Execute() override {
-		if (!claw->IsAtHome() && !hasEstablished) { // only establish once
-			log.Error(
-					"You tried to start the claw when it is not in the home position."
-							" This has been decided as unsafe. Please fix this and restart the"
-							" robot code if you would like claw functionality to be enabled");
-			claw->DisableClaw();
+		// Claw _not_ at home
+		if (!claw->IsAtHome()) {
+			// Only establish home once
+			if (!hasEstablished) {
+				DriverStation* driverStation = DriverStation::GetInstance();
+				// If we are connected to FMS
+				if (driverStation->IsFMSAttached()) {
+					// Just output a warning
+					log.Warn("Claw was not homed at the start of auto!");
+					// And let the claw work
+					claw->ZeroLiftEncoder();
+				}
+				// If we're just running tele (ie. at practice)
+				else {
+					// Output an error
+					log.Error("Claw was not homed before enabling."
+							" Please home the claw and restart"
+							" the robot code.");
+					// And kill the claw
+					claw->DisableClaw();
+				}
+			}
 		}
+		// Claw is home
+		else {
+			// All good, reset the lift encoder
+			claw->ZeroLiftEncoder();
+		}
+		hasEstablished = true;
 	}
 
 	bool IsFinished() override {
 		return true;
-	}
-
-	void End() override {
-		if (!hasEstablished) {
-			claw->ZeroLiftEncoder(); // only zero if we actually did something
-		}
-		hasEstablished = true;
-		CommandBase::End();
 	}
 
 private:
